d6dc3c07b643f6843796dd1b3febd10923e8e7cf
Adrijan Cera <acera1124si@raf.rs>
2026-06-11 02:37:25 +0200

    setup nastavak

--- server-start.bat
+++ server-start.bat
@@ -1,12 +1,24 @@
 cd "C:/"
 
-git clone "https://github.com/Ftumble/pescanik-server.git"
+echo "Povlacim server..."
+
+REM git clone "https://github.com/Ftumble/pescanik-server.git"
+
+echo "Skinut server, menjam folder"
 
 cd pescanik-server
 
+echo "Skidam biblioteke (0/2)..."
+
+npm init -y
+
+echo "Skidam biblioteke (1/2)..."
+
 npm install
 
-pause
+echo "Skinute biblioteke (2/2)"
+
+echo "Pokrecem server..."
 
 node index.js
 
